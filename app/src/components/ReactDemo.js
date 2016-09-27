27b2a8efe14f899240be40defe5f60aa94b995b7
Ajit Musalgavkar <ajit@mi.com>
2016-09-27 17:27:38 +1300

    bootstrapped an app using create-react-app package

---
+++ app/src/components/ReactDemo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ReactDemo extends Component{
+  render(){
+    return (
+      <div className="ReactDemo">
+        <h2>React Demo! Hello, world!</h2>
+      </div>
+    );
+  }
+}
+
+
+export default ReactDemo;
